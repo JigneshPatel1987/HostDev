2406c78b4714535cada993b496d5a199ccb06c9b
Aadhith Kalathil Bose <aadhi@fasteningcode.com>
2018-11-07 18:42:31 +1300

    Initial commit

---
+++ 087Nov18/Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _087Nov18
+{
+    class Function
+    {
+        public static int addnumber(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+        public int addnumber1(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+    }
+}
---
+++ 087Nov18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _087Nov18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Function.addnumber(num1, num2));
+
+            Function inst = new Function();
+            Console.WriteLine(inst.addnumber1(num1, num2));
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+    
+}
