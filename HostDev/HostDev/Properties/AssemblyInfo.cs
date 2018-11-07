ea421d5372b0f77fc64f770c3cffe75591837da9
Jignesh Patel <jigneshpatel1987@gmail.com>
2018-11-07 19:22:37 +1300

    code refactoring initial commit

--- HostDev/HostDev/Properties/AssemblyInfo.cs
+++ HostDev/HostDev/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("087Nov18")]
+[assembly: AssemblyTitle("HostDev")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("087Nov18")]
+[assembly: AssemblyProduct("HostDev")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88b52033-b4f9-43e4-b952-13ddcf647fe2")]
+[assembly: Guid("0b18fafe-fd7a-4544-aedd-714a704fdb4e")]
 
 // Version information for an assembly consists of the following four values:
 //
